2b4391f6fc8c393d5c170b478908352f18d17191
P4SCL <kidkros@hotmail.com>
2022-01-09 14:06:54 -0500

    Primer commit

---
+++ src/test/java/com/rudesfot/springsecurity/SpringSecurityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudesfot.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
